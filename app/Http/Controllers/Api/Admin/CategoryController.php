2202f92233fc26b1b7245ad77be792af4de01a3e
robbytrujillo <robbyikusuma88@gmail.com>
2024-12-09 05:38:43 +0700

    backend desa | add category controller

--- app/Http/Controllers/Api/Admin/CategoryController.php
+++ app/Http/Controllers/Api/Admin/CategoryController.php
@@ -29,34 +29,111 @@ class CategoryController extends Controller
         return new CategoryResource(true, 'List Data Categories', $categories);  
     }
 
-        /**
-        * Store a newly created resource in storage
-        * 
-        * @param \Illuminate\Http\Request  $request
-        * @return \Illuminate\Http\Response
-        */
-        public function store(Request $request) {
-            $validator = Validator::make($request->all(), [
-                'name' => 'required|unique:categories',
-            ]);
+    /**
+    * Store a newly created resource in storage
+    * 
+    * @param \Illuminate\Http\Request  $request
+    * @return \Illuminate\Http\Response
+    */
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:categories',
+        ]);
 
-            if ($validator->fails()) {
-                return response()->json($validator->errors(), 422);
-            }
-
-            // create category
-            $category = Category::create([
-                'name' => $request->name,
-                'slug' => Str::slug($request->name, '-'),
-            ]);
-
-            if ($category) {
-                // return success with Api Resource
-                return new CategoryResource(true, 'Data Category Berhasil Disimpan!', $category);
-            }
-
-            // return failed with Api Resource
-            return new CategoryResource(false, 'Data Category Gagal Disimpan!', null);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
         }
 
+        // create category
+        $category = Category::create([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name, '-'),
+        ]);
+
+        if ($category) {
+            // return success with Api Resource
+            return new CategoryResource(true, 'Data Category Berhasil Disimpan!', $category);
+        }
+
+        // return failed with Api Resource
+        return new CategoryResource(false, 'Data Category Gagal Disimpan!', null);
+    }
+
+    /**
+     * Display the specified resource.
+     * 
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) {
+        $category = Category::whereId($id)->first();
+
+        if ($category) {
+            // return success with Api Resource
+            return new CategoryResource(true, 'Detail Data Category!', $category);
+        }
+
+        // return failed with Api Resource
+        return new CategoryResource(false, 'Detail Data Category Tidak Ditemukan', $category);
+    }
+
+    /**
+     * Update the specified resource in storage
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:categories,name' . $category->id,
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // update category without image
+        $category->update([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name, '-'),
+        ]);
+
+        if ($category) {
+            // return success with Api Resource
+            return new CategoryResource(true, 'Data Category Berhasil Diupdate!', $category);
+        }
+
+        // return failed with Api Resource
+        return new CategoryResource(false, 'Data Category Gagal Diupdate!', null);
+    }
+
+    /**
+     * Remove the specified resource from storage
+     * 
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category) {
+        if ($category->delete()) {
+            // return success with Api Resource
+            return new CategoryResource(true, 'Data Category Berhasil Dihapus!', null);
+        }
+
+        // return failed with Api Resource
+        return new CategoryResource(false, 'Data Category Gagal Dihapus!', null);
+    }
+    
+    /**
+     * all
+     * 
+     * @return void
+     */
+    public function all() {
+        // get categories
+        $categories = Category::latest()->get();
+
+        // return with Api Resource
+        return new CategoryResource(true, 'List Data Categories', $categories);
+    }
 }
