696da16e1a9ef1c589df4294d3e8638cca9a5c8d
robbytrujillo <robbyikusuma88@gmail.com>
2024-12-08 10:08:32 +0700

    backend desa | add category controller

--- app/Http/Controllers/Api/Admin/CategoryController.php
+++ app/Http/Controllers/Api/Admin/CategoryController.php
@@ -11,5 +11,52 @@ use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        // get categories
+        $categories = Category::when(request()->search, function ($categories) {
+            $categories = $categories->where('name', 'like', '%' . request()->search . '%');
+        })->latest()->paginate(5);
+
+        // append query string to pagination links
+        $categories->appends(['search' => request()->search]);
+
+        // return with Api Resource
+        return new CategoryResource(true, 'List Data Categories', $categories);  
+    }
+
+        /**
+        * Store a newly created resource in storage
+        * 
+        * @param \Illuminate\Http\Request  $request
+        * @return \Illuminate\Http\Response
+        */
+        public function store(Request $request) {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|unique:categories',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 422);
+            }
+
+            // create category
+            $category = Category::create([
+                'name' => $request->name,
+                'slug' => Str::slug($request->name, '-'),
+            ]);
+
+            if ($category) {
+                // return success with Api Resource
+                return new CategoryResource(true, 'Data Category Berhasil Disimpan!', $category);
+            }
+
+            // return failed with Api Resource
+            return new CategoryResource(false, 'Data Category Gagal Disimpan!', null);
+        }
+
 }
