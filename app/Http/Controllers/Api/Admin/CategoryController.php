c60abb55a5f6d681999f29806e10e2bdf92c29b4
robbytrujillo <robbyikusuma88@gmail.com>
2024-12-08 07:52:54 +0700

    backend desa | add category controller

---
+++ app/Http/Controllers/Api/Admin/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\CategoryResource;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+    //
+}
