9a69460b8efee90c0537a523cffde850c23bd665
robbytrujillo <robbyikusuma88@gmail.com>
2024-12-09 23:23:51 +0700

    backend desa | add post controller

---
+++ app/Http/Controllers/Api/Admin/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Models\Post;
+use Illuminate\Support\Str;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    //
+}
