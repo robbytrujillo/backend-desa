9e7b7968d8c31e46c3eddb0d56b358cbf495653a
robbytrujillo <robbyikusuma88@gmail.com>
2024-12-10 10:39:29 +0700

    backend desa | add post controller

--- app/Http/Controllers/Api/Admin/PostController.php
+++ app/Http/Controllers/Api/Admin/PostController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Models\Post;
 use Illuminate\Support\Str;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
@@ -13,30 +13,31 @@ use Illuminate\Support\Facades\Validator;
 class PostController extends Controller
 {
     /**
-     * Display a listing of the resource
-     * 
+     * Display a listing of the resource.
+     *
      * @return \Illuminate\Http\Response
      */
-    public function index() {
+    public function index()
+    {
         $posts = Post::with('user', 'category')->when(request()->search, function ($posts) {
-            $posts = $posts->where('title', 'like' . request()->search . '%');
+            $posts = $posts->where('title', 'like', '%' . request()->search . '%');
         })->where('user_id', auth()->user()->id)->latest()->paginate(5);
-    
 
-    // append query string to pagination links
-    $posts->appends(['search' => request()->search]);
-    
-    // return with Api Resource
-    return new PostResource(true, 'List Data Posts', $posts);
+        //append query string to pagination links
+        $posts->appends(['search' => request()->search]);
+
+        //return with Api Resource
+        return new PostResource(true, 'List Data Posts', $posts);
     }
 
     /**
      * Store a newly created resource in storage.
-     * 
-     * @param \Illuminate\Http\Request $request
+     *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'image'         => 'required|image|mimes:jpeg,jpg,png|max:2000',
             'title'         => 'required|unique:posts',
@@ -48,55 +49,59 @@ class PostController extends Controller
             return response()->json($validator->errors(), 422);
         }
 
-        // upload image
+        //upload image
         $image = $request->file('image');
         $image->storeAs('public/posts', $image->hashName());
 
         $post = Post::create([
-            'image'         => $image->hashName(),
-            'title'         => $request->title,
-            'slug'          => Str::slug($request->title, '-'),
-            'category_id'   => $request->category_id,
-            'user_id'       => auth()->guard('api')->user()->id,
-            'content'       => $request->content
+            'image'       => $image->hashName(),
+            'title'       => $request->title,
+            'slug'        => Str::slug($request->title, '-'),
+            'category_id' => $request->category_id,
+            'user_id'     => auth()->guard('api')->user()->id,
+            'content'     => $request->content
         ]);
 
+
         if ($post) {
-            // return success with Api Resource
+            //return success with Api Resource
             return new PostResource(true, 'Data Post Berhasil Disimpan!', $post);
         }
 
-        // return failed with Api Resource
+        //return failed with Api Resource
         return new PostResource(false, 'Data Post Gagal Disimpan!', null);
     }
 
     /**
-     * Display the specified resource
-     * 
-     * @param int $id
-     * @return \Illuminate\Http\Resources
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function show($id) {
+    public function show($id)
+    {
         $post = Post::with('category')->whereId($id)->first();
 
         if ($post) {
-            // return success with Api Resource
+            //return success with Api Resource
             return new PostResource(true, 'Detail Data Post!', $post);
         }
 
-        // return failed with Api Resource
-        return new PostResource(false, 'Detail Data Post Tidak Ditemukan!', null);
+        //return failed with Api Resource
+        return new PostResource(false, 'Detail Data Post Tidak DItemukan!', null);
     }
 
     /**
-     * Display the specified resource in storage
-     * 
-     * @param int $id
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Post $post) {
+    public function update(Request $request, Post $post)
+    {
         $validator = Validator::make($request->all(), [
-            'title'         => 'required|unique:tposts,title,' .$post->id,
+            'title'         => 'required|unique:posts,title,' . $post->id,
             'category_id'   => 'required',
             'content'       => 'required'
         ]);
@@ -104,5 +109,61 @@ class PostController extends Controller
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
+
+        //check image update
+        if ($request->file('image')) {
+
+            //remove old image
+            Storage::disk('local')->delete('public/posts/' . basename($post->image));
+
+            //upload new image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            $post->update([
+                'image'       => $image->hashName(),
+                'title'       => $request->title,
+                'slug'        => Str::slug($request->title, '-'),
+                'category_id' => $request->category_id,
+                'user_id'     => auth()->guard('api')->user()->id,
+                'content'     => $request->content
+            ]);
+        }
+
+        $post->update([
+            'title'       => $request->title,
+            'slug'        => Str::slug($request->title, '-'),
+            'category_id' => $request->category_id,
+            'user_id'     => auth()->guard('api')->user()->id,
+            'content'     => $request->content
+        ]);
+
+        if ($post) {
+            //return success with Api Resource
+            return new PostResource(true, 'Data Post Berhasil Diupdate!', $post);
+        }
+
+        //return failed with Api Resource
+        return new PostResource(false, 'Data Post Gagal Disupdate!', null);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //remove image
+        Storage::disk('local')->delete('public/posts/' . basename($post->image));
+
+        if ($post->delete()) {
+            //return success with Api Resource
+            return new PostResource(true, 'Data Post Berhasil Dihapus!', null);
+        }
+
+        //return failed with Api Resource
+        return new PostResource(false, 'Data Post Gagal Dihapus!', null);
     }
 }
