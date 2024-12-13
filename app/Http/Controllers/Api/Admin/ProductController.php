fd54ead0beef97040f5492f2a7960f87fe261a20
robbytrujillo <robbyikusuma88@gmail.com>
2024-12-13 11:02:24 +0700

    backend desa | add page resource

--- app/Http/Controllers/Api/Admin/ProductController.php
+++ app/Http/Controllers/Api/Admin/ProductController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api\Admin;
 
 use App\Models\Product;
 use Illuminate\Support\Str;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Resources\ProductResource;
 use Illuminate\Support\Facades\Validator;
@@ -14,169 +14,174 @@ class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
-     * 
+     *
      * @return \Illuminate\Http\Response
      */
-    public function index() {
-        // get products
+    public function index()
+    {
+        //get products
         $products = Product::when(request()->search, function ($products) {
             $products = $products->where('title', 'like', '%' . request()->search . '%');
         })->latest()->paginate(5);
 
-        // append query string to pagination links
-        $products->appennds(['search' => request()->search]);
+        //append query string to pagination links
+        $products->appends(['search' => request()->search]);
 
-        // return with Api Resource
+        //return with Api Resource
         return new ProductResource(true, 'List Data Products', $products);
     }
 
     /**
-     * Store a newly created resource in storage
-     * 
-     * @param \Illuminate\Http\Request $request
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $validator = Validator::make($request->all(), [
-            'image'     => 'required|mimes:jpeg,jpg,png|max:2000',
-            'title'     => 'required',
-            'content'   => 'required',
-            'owner'     => 'required',
-            'price'     => 'required',
-            'address'   => 'required',
-            'phone'     => 'required',
+            'image'    => 'required|mimes:jpeg,jpg,png|max:2000',
+            'title'    => 'required',
+            'content'  => 'required',
+            'owner'    => 'required',
+            'price'    => 'required',
+            'address'  => 'required',
+            'phone'    => 'required',
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
-        // upload image
+        //upload image
         $image = $request->file('image');
-        $image->storeAs('pubic/products', $image->hashName());
+        $image->storeAs('public/products', $image->hashName());
 
-        // create Product
+        //create Product
         $product = Product::create([
-            'image'     => $image->hashName(),
-            'title'     => $request->title,
-            'slug'      => Str::slug($request->title, '-'),
-            'content'   => $request->content,
-            'owner'     => $request->owner,
-            'price'     => $request->price,
-            'address'   => $request->address,
-            'phone'     => $request->phone,
-            'user_id'   => auth()->guard('api')->user()->id,
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'slug'  => Str::slug($request->title, '-'),
+            'content' => $request->content,
+            'owner' => $request->owner,
+            'price' => $request->price,
+            'address' => $request->address,
+            'phone' => $request->phone,
+            'user_id'     => auth()->guard('api')->user()->id,
         ]);
 
         if ($product) {
-            // return success with Api Resource
-            return new ProductResource(true, 'Data Product Berhasil Disimpan', $product);
+            //return success with Api Resource
+            return new ProductResource(true, 'Data Product Berhasil Disimpan!', $product);
         }
 
-        // return failed with Api Resource
-        return new ProductResource(false, 'Data Product Gagal Disimpan', null);
+        //return failed with Api Resource
+        return new ProductResource(false, 'Data Product Gagal Disimpan!', null);
     }
 
     /**
-     * Display the specified resource
-     * 
-     * @param int $id
+     * Display the specified resource.
+     *
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id) {
+    public function show($id)
+    {
         $product = Product::whereId($id)->first();
 
         if ($product) {
-            // return success with Api Resource
+            //return success with Api Resource
             return new ProductResource(true, 'Detail Data Product!', $product);
         }
 
-        // return failed with Api Resource
-        return new ProductResource(false, 'Detail Data Product Tidak Ditemukan!', null);
+        //return failed with Api Resource
+        return new ProductResource(false, 'Detail Data Product Tidak DItemukan!', null);
     }
 
     /**
-     * Update the spesified resource in storage. 
-     * 
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Product $product) {
-        $validator = Validator::make($request->all(),[
-            'title'     => 'required',
-            'content'   => 'required',
-            'owner'     => 'required',
-            'price'     => 'required',
-            'address'   => 'required',
-            'phone'     => 'required',
+    public function update(Request $request, Product $product)
+    {
+        $validator = Validator::make($request->all(), [
+            'title'    => 'required',
+            'content'  => 'required',
+            'owner'    => 'required',
+            'price'    => 'required',
+            'address'  => 'required',
+            'phone'    => 'required',
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
-        // check image update
+        //check image update
         if ($request->file('image')) {
 
-            // remove old image
+            //remove old image
             Storage::disk('local')->delete('public/products/' . basename($product->image));
-        
-            // upload new image
+
+            //upload new image
             $image = $request->file('image');
             $image->storeAs('public/products', $image->hashName());
 
-            // update Product with new image
+            //update Product with new image
             $product->update([
-                'image'     => $image->hashName(),
-                'title'     => $request->title,
-                'slug'      => Str::slug($request->title, '-'),
-                'content'   => $request->content,
-                'owner'     => $request->owner,
-                'price'     => $request->price,
-                'address'   => $request->address,
-                'phone'     => $request->phone,
-                'user_id'   => auth()->guard('api')->user()->id,
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'slug'  => Str::slug($request->title, '-'),
+                'content' => $request->content,
+                'owner' => $request->owner,
+                'price' => $request->price,
+                'address' => $request->address,
+                'phone' => $request->phone,
+                'user_id'     => auth()->guard('api')->user()->id,
             ]);
         }
 
-        // update Product without image
+        //update Product without image
         $product->update([
-            'title'     => $request->title,
-            'slug'      => Str::slug($request->title, '-'),
-            'content'   => $request->content,
-            'owner'     => $request->owner,
-            'price'     => $request->price,
-            'address'   => $request->address,
-            'phone'     => $request->phone,
-            'user_id'   => auth()->guard('api')->user()->id,
+            'title' => $request->title,
+            'slug'  => Str::slug($request->title, '-'),
+            'content' => $request->content,
+            'owner' => $request->owner,
+            'price' => $request->price,
+            'address' => $request->address,
+            'phone' => $request->phone,
+            'user_id'     => auth()->guard('api')->user()->id,
         ]);
 
         if ($product) {
-            // return success with Api Resource
+            //return success with Api Resource
             return new ProductResource(true, 'Data Product Berhasil Diupdate!', $product);
         }
 
-        // return failed with Api Resource
+        //return failed with Api Resource
         return new ProductResource(false, 'Data Product Gagal Diupdate!', null);
     }
 
     /**
      * Remove the specified resource from storage.
-     * 
-     * @param int $id
+     *
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product) {
-        // remove image
+    public function destroy(Product $product)
+    {
+        //remove image
         Storage::disk('local')->delete('public/products/' . basename($product->image));
 
         if ($product->delete()) {
-            // return success with Api Resource
+            //return success with Api Resource
             return new ProductResource(true, 'Data Product Berhasil Dihapus!', null);
         }
 
-        // return failed with Api Resource
+        //return failed with Api Resource
         return new ProductResource(false, 'Data Product Gagal Dihapus!', null);
     }
 }
---
+++ app/Http/Resources/PageResource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PageResource extends JsonResource
+{
+    public $status;
+    public $message;
+
+    /**
+     * _construct
+     * 
+     * @param mixed $status
+     * @param mixed $message
+     * @param mixed $resource
+     * @return void
+     */
+    public function __construct($status, $message, $resource) {
+        parent::__construct($resource);
+        $this->status = $status;
+        $this->message = $message;
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+            'status'    => $this->status,
+            'message'   => $this->message,
+            'data'      => $this->resource,
+        ];
+    }
+}
