93faf287c36951a3cb2325e0ea43939dff5f5c16
robbytrujillo <robbyikusuma88@gmail.com>
2024-12-15 02:22:15 +0700

    backend desa | add page controller

--- app/Http/Controllers/Api/Admin/PageController.php
+++ app/Http/Controllers/Api/Admin/PageController.php
@@ -18,7 +18,7 @@ class PageController extends Controller
      */
     public function index() {
         // get pages
-        $pages = Page::when(request()->searc, function ($pages) {
+        $pages = Page::when(request()->search, function ($pages) {
             $pages = $pages->where('title', 'like', '%' . request()->search . '%');
         })->latest()->paginate(5);
 
@@ -28,4 +28,39 @@ class PageController extends Controller
         // return with Api Resource
         return new PageResource(true, 'List Data Pages', $pages);
     }
+
+    /**
+     * Store a newly created resource in storage.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) { 
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // create page
+        $page = Page::create([
+            'title' => $request->title,
+            'slug' => Str::slug($request->title),
+            'content' => $request->content,
+            'user_id' => auth()->guard('api')->user()->id
+        ]);
+
+        if ($page) {
+            // return success with Api Resource
+            return new PageResource(true, 'Data Page Berhasil Disimpan', $page);
+        }
+
+        // return failed with Api Resource
+        return new PageResource(false, 'Data Page Gagal Disimpan!', null);   
+    }
+
+    
 }
