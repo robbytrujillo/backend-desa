09afd2cddaf4ad1388049c02aeb6540549dd47a1
robbytrujillo <robbyikusuma88@gmail.com>
2024-12-28 12:31:27 +0700

    backend desa | add Controller and Route Api Photos Public

---
+++ app/Http/Controllers/Api/Public/PhotoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Public;
+
+use App\Models\Photo;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PhotoResource;
+use Illuminate\Http\Request;
+
+class PhotoController extends Controller
+{
+    //
+}
